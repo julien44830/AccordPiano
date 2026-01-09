b44f5e4817a209f0c49cdcd21f00d65d0e53f7d1
julien44830 <deniaudjulien0@gmail.com>
2026-01-09 21:47:07 +0100

    ajustement du texte des balises et des atribut pour la securité et les perf

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,10 @@ export default function Footer() {
         >
             <div className="mx-auto max-w-6xl px-4 py-10 grid gap-4 md:grid-cols-2 relative">
                 <a
-                    className="absolute right-5 top-38 md:top-35"
+                    className="absolute right-5 top-60 md:top-40"
                     href="https://www.artisanat.fr/metiers/labels-qualifications/qualite-artisan"
-                    target="blanc"
+                    target="_blank"
+                    rel="noopener noreferrer"
                 >
                     <img
                         className="w-16 h-auto"
@@ -25,32 +26,38 @@ export default function Footer() {
                         Contact
                     </h2>
                     <p className="mt-2 text-gray-700">
-                        Basé près de Nantes, j’interviens en Loire-Atlantique
-                        (Nantes, Saint-Herblain, Rezé, Orvault) et dans les
-                        départements limitrophes. Autres régions sur devis.
+                        Basée dans le<strong> vignoble nantais</strong>,
+                        j’interviens dans toute la
+                        <strong> Loire-Atlantique</strong> (Nantes,
+                        Saint-Herblain, Rezé, Orvault, Oudon, Vallet...) et les
+                        <strong> départements limitrophes</strong>.
+                        <br />
+                        Autres régions sur devis.
                     </p>
                 </div>
 
                 {/* Bloc coordonnées */}
                 <div className="flex flex-col gap-2 md:items-end text-[#694736] ">
-                    <Link
+                    <a
                         href="mailto:pianos.elisabeth@gmail.com"
                         className="footer-a"
                     >
                         pianos.elisabeth@gmail.com
-                    </Link>
-                    <Link
+                    </a>
+                    <a
                         href="tel:+33649841503"
                         className="footer-a"
                     >
                         +33 (0)6 49 84 15 03
-                    </Link>
+                    </a>
                     {/* Bloc lien social */}
                     <div className="flex gap-4 mt-2">
-                        <Link
+                        <a
                             href="https://www.instagram.com/elisabethpanhaleuxpianos/"
-                            className="footer-a"
+                            className="footer-a top"
                             target="_blank"
+                            aria-label="Instagram"
+                            rel="noopener noreferrer"
                         >
                             <svg
                                 width="24"
@@ -61,11 +68,13 @@ export default function Footer() {
                             >
                                 <path d="M7 2C4.243 2 2 4.243 2 7v10c0 2.757 2.243 5 5 5h10c2.757 0 5-2.243 5-5V7c0-2.757-2.243-5-5-5H7zm10 2c1.654 0 3 1.346 3 3v10c0 1.654-1.346 3-3 3H7c-1.654 0-3-1.346-3-3V7c0-1.654 1.346-3 3-3h10zm-5 3.5A4.5 4.5 0 1 0 12 17a4.505 4.505 0 0 0 4.5-4.5A4.505 4.505 0 0 0 12 7.5zm0 7.3A2.8 2.8 0 1 1 12 9.2a2.8 2.8 0 0 1 0 5.6zm4.7-7.9a1.05 1.05 0 1 0 0-2.1 1.05 1.05 0 0 0 0 2.1z" />
                             </svg>
-                        </Link>
-                        <Link
+                        </a>
+                        <a
                             href="https://www.facebook.com/Elisabethpanhaleuxpianos"
                             className="footer-a"
                             target="_blank"
+                            aria-label="facebook"
+                            rel="noopener noreferrer"
                         >
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +86,7 @@ export default function Footer() {
                             >
                                 <path d="M22.675 0h-21.35C.597 0 0 .597 0 1.326v21.348C0 23.403.597 24 1.326 24H12.82v-9.294H9.692V11.01h3.128V8.309c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 1.763v2.312h3.587l-.467 3.696h-3.12V24h6.116C23.403 24 24 23.403 24 22.674V1.326C24 .597 23.403 0 22.675 0z" />
                             </svg>
-                        </Link>
+                        </a>
                     </div>
                 </div>
             </div>
